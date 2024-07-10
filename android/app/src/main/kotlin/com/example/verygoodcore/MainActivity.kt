d490624f5da8b37578970f73c574d98eebc3f966
chendjou237 <chenxhenor@gmail.com>
2024-07-10 12:05:16 +0200

    chore: Add necessary files and configurations for macOS and iOS builds

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.intellibrax
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:intellibrax/app/app.dart';
+import 'package:intellibrax/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
