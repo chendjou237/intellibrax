5f3af8ed2d73e9d53e384b5c7d7c171068405dd9
chendjou237 <chenxhenor@gmail.com>
2024-07-15 00:58:41 +0200

    correcting main

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,7 +1,34 @@
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intellibra/bootstrap.dart';
 import 'package:intellibra/src/app/app.dart';
+import 'package:intellibra/src/features/awareness/domain/awareness_repository.dart';
+import 'package:intellibra/src/features/onboarding/thememode/cubit/theme_cubit.dart';
+import 'package:intellibra/src/features/scan/presentation/cubit/scan_cubit.dart';
+
+import 'package:intellibra/src/features/awareness/presentation/cubit/awareness_cubit.dart';
 
 void main() {
-  bootstrap(() => const Intellibra());
+  bootstrap(
+    () => MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider(
+          create: (context) => AwarenessRepository(),
+        ),
+      ],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider.value(
+            value: ThemeCubit(),
+          ),
+          BlocProvider.value(value: ScanCubit()..checkBlueState()),
+          BlocProvider(
+            create: (context) => AwarenessCubit(
+              context.read<AwarenessRepository>(),
+            ),
+          ),
+        ],
+        child: const Intellibra(),
+      ),
+    ),
+  );
 }
